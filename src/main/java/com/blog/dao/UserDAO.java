3f0163d722babd75a0b75ac7697866a3984a936a
bangchiubangchiu <91598947+szhin@users.noreply.github.com>
2023-10-18 21:31:02 +0700

    Forgot method in UserDAO

--- src/main/java/com/blog/dao/UserDAO.java
+++ src/main/java/com/blog/dao/UserDAO.java
@@ -2,7 +2,9 @@ package com.blog.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.blog.connection.DatabaseUtil;
@@ -40,25 +42,86 @@ public class UserDAO implements IDAO<User> {
 
 	@Override
 	public void update(User user) {
-		// TODO Auto-generated method stub
-		
+		Connection connection = databaseUtil.getConnection();
+        String query = "UPDATE " + TABLE_NAME + " SET uname = ?, uemail = ?, upw = ?, umobile = ? WHERE id = ?";
+
+        try {
+            PreparedStatement preStatement = connection.prepareStatement(query);
+            preStatement.setString(1, user.getUname());
+            preStatement.setString(2, user.getUemail());
+            preStatement.setString(3, user.getUpw());
+            preStatement.setString(4, user.getUmobile());
+            preStatement.setInt(5, user.getId());
+            preStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 	}
 
 	@Override
 	public void delete(User user) {
-		// TODO Auto-generated method stub
-		
+		Connection connection = databaseUtil.getConnection();
+        String query = "DELETE FROM " + TABLE_NAME + " WHERE id = ?";
+
+        try {
+            PreparedStatement preStatement = connection.prepareStatement(query);
+            preStatement.setInt(1, user.getId());
+            preStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 	}
 	
 	@Override
 	public User get(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Connection connection = databaseUtil.getConnection();
+        String query = "SELECT * FROM " + TABLE_NAME + " WHERE id = ?";
+
+        try {
+            PreparedStatement preStatement = connection.prepareStatement(query);
+            preStatement.setInt(1, id);
+            ResultSet rs = preStatement.executeQuery();
+
+            if (rs.next()) {
+                User user = new User();
+                user.setId(rs.getInt("id"));
+                user.setUname(rs.getString("uname"));
+                user.setUemail(rs.getString("uemail"));
+                user.setUpw(rs.getString("upw"));
+                user.setUmobile(rs.getString("umobile"));
+
+                return user;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return null;
 	}
 	@Override
 	public List<User> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		Connection connection = databaseUtil.getConnection();
+        String query = "SELECT * FROM " + TABLE_NAME;
+        List<User> users = new ArrayList<>();
+
+        try {
+            PreparedStatement preStatement = connection.prepareStatement(query);
+            ResultSet rs = preStatement.executeQuery();
+
+            while (rs.next()) {
+                User user = new User();
+                user.setId(rs.getInt("id"));
+                user.setUname(rs.getString("uname"));
+                user.setUemail(rs.getString("uemail"));
+                user.setUpw(rs.getString("upw"));
+                user.setUmobile(rs.getString("umobile"));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return users;
 	}
 
 
